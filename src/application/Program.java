2c896a00f37b630d3e851b8776646b226c76fa77
LucasRocha <lucascaixeta98@gmail.com>
2023-10-03 14:05:04 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
+import java.util.Date;
 import java.util.Locale;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -10,6 +12,8 @@ public class Program {
 		Locale.setDefault(Locale.US);
 
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Ana", "ana@gmail.com", new Date(), 3000.0, obj);
+
+		System.out.println(seller);
 	}
 }
